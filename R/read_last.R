81223d31ddc92c7729d42a1ad5b1f85379bae34c
BJS (Benjamin Schwetz) <bjs@novozymes.com>
2019-07-20 20:58:21 +0200

    moved read last functions to fix later

---
+++ R/read_last.R
@@ -0,0 +1,66 @@
+#' Title
+#'
+#' @param channel
+#' @param parameters
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_last_data <- function(channel, parameters = list()) {
+  .compose_ts_url(
+    channel = channel,
+    request_type = "feeds",
+    last = TRUE,
+    parameters = parameters
+  ) %>%
+    httr::build_url() %>%
+    httr::GET() %>%
+    .parse_response()
+}
+
+
+#' Title
+#'
+#' @param channel
+#' @param parameters
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_last_status <- function(channel, parameters = list()) {
+  .compose_ts_url(
+    channel = channel,
+    request_type = "status",
+    last = TRUE,
+    parameters = parameters
+  ) %>%
+    httr::build_url() %>%
+    httr::GET() %>%
+    .parse_response()
+}
+
+
+#' Title
+#'
+#' @param channel
+#' @param field
+#' @param parameters
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_last_field <- function(channel, field, parameters = list()) {
+  .compose_ts_url(
+    channel = channel,
+    request_type = "fields",
+    last = TRUE,
+    field = field,
+    parameters = parameters
+  ) %>%
+    httr::build_url() %>%
+    httr::GET() %>%
+    .parse_response()
+}
